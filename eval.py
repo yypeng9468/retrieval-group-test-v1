92cf183658a525862fd41952033134396a960db6
yypeng <pengyuyan@qiniu.com>
2018-08-19 09:52:30 +0800

    add some change

--- eval.py
+++ eval.py
@@ -22,66 +22,50 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    res = []
-    a = 0
-    j = 0
-
-    h = open("../txt/query.txt.retrieval.json",'r')
-
-    k = 50
+    res_re = []
+    b = 0
+    h = open("ars.json",'r')
 
     for line in h.readlines():
-        j += 1 
         dic = json.loads(line)
         if not "error" in dic.keys():
-            t = {"url": dic["url"], "true":0, "simialr_uri":[], "differ_uri":[]}
-            a += 1
-            im_num = (dic["url"].split(".")[-2]).split('/')[-1]
-            if not k > len(dic["search_results"][0]["results"]):
-                for result in dic["search_results"][0]["results"][:k]:
-                    if ((result["id"].split('/')[4]).split('__')[0]=="eval") and (im_num in (result["id"].split('/'))[4].split('-')[0]):
-                        t["simialr_uri"].append(result["id"])
-                        t["true"] += 1
-                    else:
-                        t["differ_uri"].append(result["id"])
-                res.append(t)
-            else:
-                for result in dic["search_results"][0]["results"]:
-                    if ((result["id"].split('/')[4]).split('__')[0]=="eval") and (im_num in (result["id"].split('/'))[4].split('-')[0]):
-                        t["simialr_uri"].append(result["id"])
-                        t["true"] += 1
-                    else:
-                        t["differ_uri"].append(result["id"])
-                res.append(t)          
-
-    p = 0
-    for i in range(a):
-        p += res[i]["true"]
-        
-    precision = p/(float(a-1)*15)
-    print ("The recall percentage is %f" % recall)
-
-
-    for line in h.readlines()[:1]:
-        j += 1 
-        dic = json.loads(line)
-        
-        if not "error" in dic.keys():
-            t = {"url": dic["url"], "true":0, "simialr_uri":[], "differ_uri":[]}
-            a += 1
+            b += 1
+            t = {"url": dic["url"], "true":0, "simialr_uri":[]}
             im_num = (dic["url"].split(".")[-2]).split('/')[-1]
             for result in dic["search_results"][0]["results"]:
                 if ((result["id"].split('/')[4]).split('__')[0]=="eval") and (im_num in (result["id"].split('/'))[4].split('-')[0]):
                     t["simialr_uri"].append(result["id"])
                     t["true"] += 1
-                else:
-                    t["differ_uri"].append(result["id"])
-            res.append(t)
-
+            res_re.append(t)
+    h.close()
 
     r = 0
-    for i in range(a):
-        if res[i]["true"]>=1:
+    for i in range(b):
+        if res_re[i]["true"]>=args.top_k:
             r += 1
-    recall = r/float(a-1)
-    print ("The top-5 correct percentage is %f" % precision)
+    recall = r/float(b-1)
+    print ("The top-5 recall percentage is %f" % recall)
+
+
+    a = 0
+    k = args.top_k
+    res_pr = []
+    h = open("../txt/query.txt.retrieval.json",'r')
+    for line in h.readlines():
+        dic = json.loads(line)
+        if not "error" in dic.keys():
+            a += 1
+            t = {"url": dic["url"], "true":0, "simialr_uri":[]}
+            im_num = (dic["url"].split(".")[-2]).split('/')[-1]
+            for i,result in enumerate(dic["search_results"][0]["results"]):
+                if i <= k and ((result["id"].split('/')[4]).split('__')[0]=="eval") and (im_num in (result["id"].split('/'))[4].split('-')[0]):
+                    t["simialr_uri"].append(result["id"])
+                    t["true"] += 1
+            res_pr.append(t)
+
+    p = 0
+    for i in range(a):
+        p += res_pr[i]["true"]
+
+    precision = p/(float(a-1)*15)
+    print ("The precision percentage is %f" % precision)
